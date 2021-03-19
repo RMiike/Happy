c89190bb99c5eb9eaf0481811d6bdece5bdfd3a0
Renato Miike Alves <renato@aqua.com.br>
2021-03-19 14:47:33 -0300

    folder structure

---
+++ front-end/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles/global.css";
+function App() {
+  return (
+    <div className="App">
+      <h1>hi nlw</h1>
+    </div>
+  );
+}
+
+export default App;
