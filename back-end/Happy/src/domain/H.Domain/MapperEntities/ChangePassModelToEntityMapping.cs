dbaabf07146228d9cb6e3c5b8764de75856fe9fa
Renato Miike Alves <renato@aqua.com.br>
2021-05-29 19:07:42 -0300

    adicionado mudança de password ao backend

---
+++ back-end/Happy/src/domain/H.Domain/MapperEntities/ChangePassModelToEntityMapping.cs
@@ -0,0 +1,11 @@
+﻿using H.Domain.Entities;
+using H.Domain.Models;
+
+namespace H.Domain.MapperEntities
+{
+    public static class ChangePassModelToEntityMapping
+    {
+        public static ChangePasswordEntity ConvertToEntity(this ChangePasswordModel model)
+            => new ChangePasswordEntity(model.OldPassword, model.NewPassword, model.ConfirmPassword);
+    }
+}
